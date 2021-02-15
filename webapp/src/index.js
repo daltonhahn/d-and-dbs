7caebbc331f5f3f7c4bc567db06409feb3d1264a
daltonhahn <hahnd11@gmail.com>
2021-02-15 11:19:32 -0500

    Change order

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -7,8 +7,8 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
     <SimpleTabs />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
