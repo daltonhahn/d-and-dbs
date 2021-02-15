1937bb32115f5fa4c56dd653800e5d22ff3404ca
daltonhahn <hahnd11@gmail.com>
2021-02-15 11:12:50 -0500

    Only one default component

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -74,7 +74,7 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function SimpleTabs() {
+export function SimpleTabs() {
   const classes = useStyles();
   const [value, setValue] = React.useState(0);
 
